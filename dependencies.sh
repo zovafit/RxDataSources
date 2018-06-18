026857261128bf552369a3b69b3ab56c3f85704d
Krunoslav Zaher <krunoslav.zaher@gmail.com>
2018-06-18 21:52:23 +0200

    Fixes for Xcode 10

--- dependencies.sh
+++ dependencies.sh
@@ -1,6 +1,15 @@
-#set -e
-#if [[ ( ! -d "RxSwift/.git" ) && ( ! -d "Carthage" ) && ( ! -d 'Pods' ) ]]; then
-#    git submodule update --init --recursive --force
-#    cd RxSwift
-#    git reset origin/master --hard
-#fi
+set -e
+
+if [[ ( -f "RxSwift/Rx.xcodeproj/project.pbxproj" ) && (( -d "Carthage" ) || ( -d 'Pods' )) ]]; then
+    pushd RxSwift
+    git fetch origin --tags
+    git reset 0.0.0 --hard
+    popd
+fi
+
+if [[ ( ! -f "RxSwift/Rx.xcodeproj/project.pbxproj" ) && ( ! -d "Carthage" ) && ( ! -d 'Pods' ) ]]; then
+    git submodule update --init --recursive --force
+    pushd RxSwift
+    git reset origin/master --hard
+    popd
+fi
